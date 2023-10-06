43cfd338c929589ece1a8eaf864684fd4b4a7c6e
Grebennikov Roman <grv@dfdx.me>
2023-10-06 17:56:12 +0200

    add jvm vector incubator module flag

--- deploy/nixiesearch.sh
+++ deploy/nixiesearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euxo pipefail
-OPTS=${JAVA_OPTS:-"-Xmx1g -verbose:gc"}
+OPTS=${JAVA_OPTS:-"-Xmx1g -verbose:gc --add-modules jdk.incubator.vector"}
 
 exec /usr/bin/java $OPTS --enable-preview -jar /app/nixiesearch.jar "$@"
 
