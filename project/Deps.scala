41364d1c403765c44908eeccbddfcac1b41b4fc7
Grebennikov Roman <grv@dfdx.me>
2024-05-13 20:47:25 +0200

    bump circe to 0.14.7

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Deps {
   lazy val http4sVersion    = "1.0.0-M41"
   lazy val scalatestVersion = "3.2.18"
-  lazy val circeVersion     = "0.14.6"
+  lazy val circeVersion     = "0.14.7"
   lazy val circeYamlVersion = "1.15.0"
   lazy val fs2Version       = "3.10.2"
   lazy val luceneVersion    = "9.10.0"
