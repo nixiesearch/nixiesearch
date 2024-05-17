1f496b1e750469f721ffd33d64a4450ed10ad276
Scala Steward <scala_steward@virtuslab.com>
2024-05-17 17:24:53 +0000

    Update s3 to 2.25.54

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,6 +7,6 @@ object Deps {
   lazy val circeYamlVersion = "1.15.0"
   lazy val fs2Version       = "3.10.2"
   lazy val luceneVersion    = "9.10.0"
-  lazy val awsVersion       = "2.25.52"
+  lazy val awsVersion       = "2.25.54"
   lazy val djlVersion       = "0.27.0"
 }
