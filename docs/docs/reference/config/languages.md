007ddf6eb19cf3e69373c885f2fcc698d96c45d9
Grebennikov Roman <grv@dfdx.me>
2024-05-17 17:38:58 +0200

    add all supported languages from Lucene

---
+++ docs/docs/reference/config/languages.md
@@ -0,0 +1,79 @@
+# Language support
+
+Nixiesearch language support differs for lexical and semantic search methods:
+* for lexical search, all Lucene analyzers are supported out of the box - see [full list below](#language-support-for-lexicalhybrid-search)
+* for semantic search, language support depends on the underlying embedding model used. See some examples in a [Language support for semantic search](#language-support-for-semantic-search) below.
+
+Language can be set in the index mapping for text-like fields:
+
+```yaml
+search:
+  your-index-name:
+    fields:
+      title:
+        type: text
+        search: semantic
+        language: en # use an ISO-639-1 language code
+```
+
+> If language is not defined, a special **default** language analyzer is used - no language specific transformations are done, only ICU tokenization.
+
+## Language support for lexical/hybrid search
+
+Nixiesearch supports all languages from [Apache Lucene](https://lucene.apache.org/core/8_5_1/analyzers-common/index.html) library:
+
+| Language            | ISO 639-1 code |
+|---------------------|----------------|
+| Generic             | default        |
+| English             | en             |
+| Arabic              | ar             |
+| Bulgarian           | br             |
+| Bengali             | br             |
+| Brazilian Portugese | br             |
+| Catalan             | br             |
+| Simplified Chinese  | zh             |
+| Czech               | cz             |
+| Danish              | da             |
+| German              | de             |
+| Greek               | el             |
+| Spanish             | es             |
+| Estonian            | et             |
+| Basque              | eu             |
+| Persian             | fa             |
+| Finnish             | fi             |
+| French              | fr             |
+| Irish               | ga             |
+| Hindi               | hi             |
+| Hungarian           | hu             |
+| Armenian            | hy             |
+| Indonesian          | id             |
+| Italian             | it             |
+| Lithuanian          | lt             |
+| Latvian             | lv             |
+| Dutch               | nl             |
+| Norwegian           | no             |
+| Portuguese          | pt             |
+| Romanian            | ro             |
+| Russian             | ru             |
+| Serbian             | sr             |
+| Swedish             | sv             |
+| Thai                | th             |
+| Turkish             | tr             |
+| Japanese            | ja             |
+| Polish              | po             |
+| Korean              | kr             |
+| Tamil               | ta             |
+| Ukrainian           | ua             |
+
+> If your language is not included in the list, please file a GitHub issie: https://github.com/nixiesearch/nixiesearch/issues
+
+## Language support for semantic search
+
+Language support for semantic search fully depends on the embedding model used:
+
+* [sentence-transformers/all-MiniLM-L6-v2](): English
+* [BAAI/bge-large-en-v1.5](https://huggingface.co/BAAI/bge-large-en-v1.5): English, a special version for [Chinese](https://huggingface.co/BAAI/bge-large-zh-v1.5)
+* [Alibaba-NLP/gte-base-en-v1.5](https://huggingface.co/Alibaba-NLP/gte-base-en-v1.5) English
+* [intfloat/multilingual-e5-large](https://huggingface.co/intfloat/multilingual-e5-large): English, Chinese and all languages from [MIRACL dataset](https://huggingface.co/datasets/miracl/miracl)
+
+So if your target language is English, you can choose almost any model from the [MTEB leaderboard](https://huggingface.co/spaces/mteb/leaderboard) you like. For multilingual model, the [intfloat/multilingual-e5-large](https://huggingface.co/intfloat/multilingual-e5-large) is recommended.
--- src/main/scala/ai/nixiesearch/config/mapping/Language.scala
+++ src/main/scala/ai/nixiesearch/config/mapping/Language.scala
@@ -1,40 +1,122 @@
 package ai.nixiesearch.config.mapping
 
+import ai.nixiesearch.core.Error.UserError
+import ai.nixiesearch.core.Logging
+import cats.effect.IO
+
 import scala.util.{Failure, Success}
 import io.circe.{Decoder, Encoder}
 import io.circe.Json
+import org.apache.lucene.analysis.ar.ArabicAnalyzer
+import org.apache.lucene.analysis.bg.BulgarianAnalyzer
+import org.apache.lucene.analysis.bn.BengaliAnalyzer
+import org.apache.lucene.analysis.br.BrazilianAnalyzer
+import org.apache.lucene.analysis.ca.CatalanAnalyzer
+import org.apache.lucene.analysis.ckb.SoraniAnalyzer
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer
 import org.apache.lucene.analysis.{Analyzer, CharArraySet, Tokenizer}
 import org.apache.lucene.analysis.core.KeywordAnalyzer
+import org.apache.lucene.analysis.cz.CzechAnalyzer
+import org.apache.lucene.analysis.da.DanishAnalyzer
+import org.apache.lucene.analysis.de.GermanAnalyzer
+import org.apache.lucene.analysis.el.GreekAnalyzer
 import org.apache.lucene.analysis.en.EnglishAnalyzer
+import org.apache.lucene.analysis.es.SpanishAnalyzer
+import org.apache.lucene.analysis.et.EstonianAnalyzer
+import org.apache.lucene.analysis.eu.BasqueAnalyzer
+import org.apache.lucene.analysis.fa.PersianAnalyzer
+import org.apache.lucene.analysis.fi.FinnishAnalyzer
+import org.apache.lucene.analysis.fr.FrenchAnalyzer
+import org.apache.lucene.analysis.ga.IrishAnalyzer
+import org.apache.lucene.analysis.gl.GalicianAnalyzer
+import org.apache.lucene.analysis.hi.HindiAnalyzer
+import org.apache.lucene.analysis.hu.HungarianAnalyzer
+import org.apache.lucene.analysis.hy.ArmenianAnalyzer
 import org.apache.lucene.analysis.icu.segmentation.ICUTokenizer
+import org.apache.lucene.analysis.id.IndonesianAnalyzer
+import org.apache.lucene.analysis.it.ItalianAnalyzer
+import org.apache.lucene.analysis.ja.JapaneseAnalyzer
+import org.apache.lucene.analysis.ko.KoreanAnalyzer
+import org.apache.lucene.analysis.lt.LithuanianAnalyzer
+import org.apache.lucene.analysis.lv.LatvianAnalyzer
+import org.apache.lucene.analysis.nl.DutchAnalyzer
+import org.apache.lucene.analysis.no.NorwegianAnalyzer
+import org.apache.lucene.analysis.pl.PolishAnalyzer
+import org.apache.lucene.analysis.pt.PortugueseAnalyzer
+import org.apache.lucene.analysis.ro.RomanianAnalyzer
+import org.apache.lucene.analysis.ru.RussianAnalyzer
+import org.apache.lucene.analysis.sr.SerbianAnalyzer
 import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.analysis.sv.SwedishAnalyzer
+import org.apache.lucene.analysis.ta.TamilAnalyzer
+import org.apache.lucene.analysis.th.ThaiAnalyzer
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+import org.apache.lucene.analysis.tr.TurkishAnalyzer
+import org.apache.lucene.analysis.uk.UkrainianMorfologikAnalyzer
 
 import java.util.Locale
 import scala.collection.mutable.ArrayBuffer
 
-sealed trait Language {
-  def analyzer: Analyzer
-
+enum Language(val code: String, makeAnalyzer: => Analyzer) extends Logging {
+  lazy val analyzer = {
+    logger.debug(s"created analyzer for language '$code'")
+    makeAnalyzer
+  }
+  case Generic            extends Language("default", new StandardAnalyzer())
+  case English            extends Language("en", new EnglishAnalyzer())
+  case Arabic             extends Language("ar", new ArabicAnalyzer())
+  case Bulgarian          extends Language("bg", new BulgarianAnalyzer())
+  case Bengali            extends Language("bn", new BengaliAnalyzer())
+  case BrazilianPortugese extends Language("br", new BrazilianAnalyzer())
+  case Catalan            extends Language("ca", new CatalanAnalyzer())
+  case Chinese            extends Language("zh", new SmartChineseAnalyzer())
+  case SoraniKurdish      extends Language("ckb", new SoraniAnalyzer())
+  case Czech              extends Language("cz", new CzechAnalyzer())
+  case Danish             extends Language("da", new DanishAnalyzer())
+  case German             extends Language("de", new GermanAnalyzer())
+  case Greek              extends Language("el", new GreekAnalyzer())
+  case Spanish            extends Language("es", new SpanishAnalyzer())
+  case Estonian           extends Language("et", new EstonianAnalyzer())
+  case Basque             extends Language("eu", new BasqueAnalyzer())
+  case Persian            extends Language("fa", new PersianAnalyzer())
+  case Finnish            extends Language("fi", new FinnishAnalyzer())
+  case French             extends Language("fr", new FrenchAnalyzer())
+  case Irish              extends Language("ga", new IrishAnalyzer())
+  case Galician           extends Language("gl", new GalicianAnalyzer())
+  case Hindi              extends Language("hi", new HindiAnalyzer())
+  case Hungarian          extends Language("hu", new HungarianAnalyzer())
+  case Armenian           extends Language("hy", new ArmenianAnalyzer())
+  case Indonesian         extends Language("id", new IndonesianAnalyzer())
+  case Italian            extends Language("it", new ItalianAnalyzer())
+  case Lithuanian         extends Language("lt", new LithuanianAnalyzer())
+  case Latvian            extends Language("lv", new LatvianAnalyzer())
+  case Dutch              extends Language("nl", new DutchAnalyzer())
+  case Norwegian          extends Language("no", new NorwegianAnalyzer())
+  case Portuguese         extends Language("pt", new PortugueseAnalyzer())
+  case Romanian           extends Language("ro", new RomanianAnalyzer())
+  case Russian            extends Language("ru", new RussianAnalyzer())
+  case Serbian            extends Language("sr", new SerbianAnalyzer())
+  case Swedish            extends Language("sv", new SwedishAnalyzer())
+  case Thai               extends Language("th", new ThaiAnalyzer())
+  case Turkish            extends Language("tr", new TurkishAnalyzer())
+  case Japanese           extends Language("jp", new JapaneseAnalyzer())
+  case Polish             extends Language("pl", new PolishAnalyzer())
+  case Korean             extends Language("kr", new KoreanAnalyzer())
+  case Tamil              extends Language("ta", new TamilAnalyzer())
+  case Ukrainian          extends Language("ua", new UkrainianMorfologikAnalyzer())
 }
 
-object Language {
-  case object Generic extends Language {
-    override val analyzer = new StandardAnalyzer()
-  }
-  case object English extends Language {
-    override val analyzer = new EnglishAnalyzer()
-  }
+object Language extends Logging {
+  val languages                               = Language.values.map(lang => lang.code -> lang).toMap
+  def forCode(code: String): Option[Language] = languages.get(code)
 
-  given languageTypeDecoder: Decoder[Language] = Decoder.decodeString.emapTry {
-    case "en" | "english" => Success(English)
-    case "generic"        => Success(Generic)
-    case other            => Failure(new Exception(s"language '$other' is not supported. maybe try 'english'?"))
-  }
+  given languageTypeDecoder: Decoder[Language] = Decoder.decodeString.emapTry(code =>
+    languages.get(code) match {
+      case Some(lang) => Success(lang)
+      case None       => Failure(new Exception(s"language '$code' is not supported. maybe try 'english'?"))
+    }
+  )
 
-  given languageTypeEncoder: Encoder[Language] = Encoder.instance {
-    case English => Json.fromString("english")
-    case Generic => Json.fromString("generic")
-  }
+  given languageTypeEncoder: Encoder[Language] = Encoder.instance(lang => Json.fromString(lang.code))
 
 }
