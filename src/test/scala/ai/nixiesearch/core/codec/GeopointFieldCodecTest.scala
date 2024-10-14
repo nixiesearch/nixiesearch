874e71eb484de53f4e68f4e56408afad58c6166f
Grebennikov Roman <grv@dfdx.me>
2024-10-14 20:12:40 +0200

    unbreak GeopointFieldCodecTest

--- src/test/scala/ai/nixiesearch/core/codec/GeopointFieldCodecTest.scala
+++ src/test/scala/ai/nixiesearch/core/codec/GeopointFieldCodecTest.scala
@@ -1,10 +1,18 @@
 package ai.nixiesearch.core.codec
 
+import ai.nixiesearch.config.FieldSchema.GeopointFieldSchema
+import ai.nixiesearch.core.Field.GeopointField
+import org.apache.lucene.document.{Document, StoredField}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class GeopointFieldCodecTest extends AnyFlatSpec with Matchers {
   it should "roundtrip stored geopoints" in {
-    fail("TODO")
+    val doc    = new Document()
+    val schema = GeopointFieldSchema("location", store = true)
+    GeopointFieldCodec.write(GeopointField("location", 1, 2), schema, doc)
+    val bytes   = doc.getField("location").asInstanceOf[StoredField]
+    val decoded = GeopointFieldCodec.read(schema, bytes.binaryValue().bytes)
+    decoded shouldBe Right(GeopointField("location", 1, 2))
   }
 }
