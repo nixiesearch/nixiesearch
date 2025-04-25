9c43a51cf9854edb3fa831d0a3d79bad6e0d9449
Grebennikov Roman <grv@dfdx.me>
2025-04-25 14:56:12 +0200

    add tests for dis_max, bool, knn and semantic queries

---
+++ src/test/scala/ai/nixiesearch/api/query/KnnQueryTest.scala
@@ -0,0 +1,53 @@
+package ai.nixiesearch.api.query
+
+import ai.nixiesearch.api.query.retrieve.{KnnQuery, MatchQuery}
+import ai.nixiesearch.config.FieldSchema.TextFieldSchema
+import ai.nixiesearch.config.InferenceConfig
+import ai.nixiesearch.config.mapping.FieldName.StringName
+import ai.nixiesearch.config.mapping.SearchParams
+import ai.nixiesearch.config.mapping.SearchParams.{LexicalParams, SemanticParams}
+import ai.nixiesearch.core.Document
+import ai.nixiesearch.core.field.TextField
+import ai.nixiesearch.core.nn.ModelRef
+import ai.nixiesearch.core.nn.model.embedding.EmbedModel.TaskType.Query
+import ai.nixiesearch.util.{SearchTest, TestIndexMapping, TestInferenceConfig}
+import org.scalatest.matchers.should.Matchers
+import cats.effect.unsafe.implicits.global
+
+import scala.util.Try
+
+class KnnQueryTest extends SearchTest with Matchers {
+  override def inference: InferenceConfig = TestInferenceConfig.semantic()
+  val mapping = TestIndexMapping(
+    "test",
+    fields = List(
+      TextFieldSchema(name = StringName("_id"), filter = true),
+      TextFieldSchema(
+        name = StringName("title"),
+        search = SearchParams(semantic = Some(SemanticParams(model = ModelRef("text"))))
+      )
+    )
+  )
+  val docs = List(
+    Document(List(TextField("_id", "1"), TextField("title", "red dress"))),
+    Document(List(TextField("_id", "2"), TextField("title", "white dress"))),
+    Document(List(TextField("_id", "3"), TextField("title", "red pajama")))
+  )
+
+  it should "select matching documents for a knn query" in withIndex { index =>
+    {
+      val queryEmbed =
+        index.indexer.index.models.embedding.encode(ModelRef("text"), Query, "lady in red").unsafeRunSync()
+      val docs = index.search(KnnQuery("title", queryEmbed, 1))
+      docs shouldBe List("1")
+    }
+  }
+
+  it should "fail on dim mismatch" in withIndex { index =>
+    {
+      val result = Try(index.search(KnnQuery("title", Array(0.0f, 0.0f), 1)))
+      result.isFailure shouldBe true
+    }
+  }
+
+}
