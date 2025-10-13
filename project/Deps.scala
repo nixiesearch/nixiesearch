4e537f31af28105741ddac9c1f1dac0ff65bd097
Scala Steward <scala_steward@virtuslab.com>
2025-10-13 16:39:33 +0000

    Update s3 to 2.35.5

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   lazy val fs2Version         = "3.12.2"
   lazy val fs2CompressVersion = "2.3.2"
   lazy val luceneVersion      = "10.3.0"
-  lazy val awsVersion         = "2.32.33"
+  lazy val awsVersion         = "2.35.5"
   lazy val djlVersion         = "0.34.0"
   lazy val onnxRuntimeVersion = "1.23.0"
   lazy val llamacppVersion    = "0.0.4-b5604"
