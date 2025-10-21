3ad396cf88580acd28d1a6afeed01689f5eeb8e1
Scala Steward <scala_steward@virtuslab.com>
2025-10-21 17:38:14 +0000

    Update api to 0.35.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   lazy val fs2CompressVersion = "2.3.2"
   lazy val luceneVersion      = "10.3.0"
   lazy val awsVersion         = "2.32.33"
-  lazy val djlVersion         = "0.34.0"
+  lazy val djlVersion         = "0.35.0"
   lazy val onnxRuntimeVersion = "1.23.0"
   lazy val llamacppVersion    = "0.0.4-b5604"
   lazy val prometheusVersion  = "1.4.1"
